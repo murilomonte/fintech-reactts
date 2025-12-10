f380ead4a7e97aea07c230b977ce499ee9fa08fe
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-12-09 22:24:32 -0300

    feat: dataContext implementado

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import Resumo from "./Pages/Resumo";
 import Sidenav from "./Components/Sidenav";
 import Header from "./Components/Header";
+import { DataContextProvider } from "./Context/DataContext";
 
 const App = () => {
   return (
-    <div>
-      <Sidenav />
+    <DataContextProvider>
       <div>
-        <Header />
-        <Resumo />
+        <Sidenav />
+        <div>
+          <Header />
+          <Resumo />
+        </div>
       </div>
-    </div>
+    </DataContextProvider>
   );
 };
 
